cae4a5d43a19d56e040654cf6e95ecea67157a11
Your Name <you@example.com>
2025-11-07 21:44:12 +0100

    kolejne zminay pod hosting na renderze

--- server.js
+++ server.js
@@ -10,11 +10,18 @@ app.use(bodyParser.json());
 const booksRoutes = require('./backend/routes/books');
 const usersRoutes = require('./backend/routes/users'); 
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 
 // Użyj tras
 app.use('/api/books', booksRoutes);
 app.use('/api', usersRoutes); 
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
