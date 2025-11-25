9505aa89e75f90fe4dd84b9cd95ead5ea8f5d68f
Your Name <you@example.com>
2025-11-25 15:43:11 +0100

    lekkie zmiany z portem pod rendera + dodany plik - .gitignore

--- server.js
+++ server.js
@@ -19,4 +19,6 @@ app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, () => console.log('Server działa na porcie 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server działa na porcie ${PORT}`));
+
